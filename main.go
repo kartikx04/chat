468b5ba415a5e70fa2ff8a2e089785571fa01423
kartik Verma <kartikx04@gmail.com>
2026-01-23 05:18:19 +0530

    created server instance to control server behavior.

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func health(w http.ResponseWriter, r *http.Request) {
@@ -11,8 +13,17 @@ func health(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	mux := http.NewServeMux()
+	srv := &http.Server{
+		Handler:      mux,
+		Addr:         "127.0.0.1:8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	fmt.Printf("listening on port: %s\n", srv.Addr)
+
 	mux.HandleFunc("/health", health)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("server error: %v", err)
 	}
 }
