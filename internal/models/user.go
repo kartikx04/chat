dfdc6dea6fe4ec364695b6486a340727e19233ca
Siddharth <kartikx04@gmail.com>
2026-03-06 04:53:24 +0530

    fixed models/user.go

--- internal/models/user.go
+++ internal/models/user.go
@@ -9,16 +9,16 @@ import (
 type User struct {
 	Id        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
 	AuthOId   string    `gorm:"unique" json:"auth_o_id"`
-	Email     string    `json:"email"`
-	Username  string    `json:"username"`
+	Email     string    `gorm:"unique" json:"email"`
+	Username  string    `gorm:"unique" json:"username"`
 	Picture   string    `json:"picture"`
 	Role      string    `json:"role"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 }
 
 type OAuthData struct {
 	Id            string `gorm:"primaryKey" json:"id"`
-	Email         string `json:"email"`
+	Email         string `gorm:"unique" json:"email"`
 	VerifiedEmail bool   `json:"verified_email"`
 	Picture       string `json:"picture"`
 }
