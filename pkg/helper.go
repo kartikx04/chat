730ca3ead774807e0a9187c26a22175690a0b0cf
kartik Verma <kartikx04@gmail.com>
2026-04-22 17:49:10 +0530

    changed loadfile func according to production

--- pkg/helper.go
+++ pkg/helper.go
@@ -1,20 +1,21 @@
 package pkg
 
 import (
+	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func InitEnv() {
-	// Load .env only in local/dev
-	if os.Getenv("ENV") == "" || os.Getenv("ENV") == "development" {
-		_ = godotenv.Load()
-	}
+	// Load .env only in development (won't error if file doesn't exist)
+	_ = godotenv.Load()
 }
+
 func LoadFile(key string) string {
-	if os.Getenv("ENV") != "production" {
-		_ = godotenv.Load()
+	val := os.Getenv(key)
+	if val == "" {
+		log.Fatalf("Missing required environment variable: %s", key)
 	}
-	return os.Getenv(key)
+	return val
 }
