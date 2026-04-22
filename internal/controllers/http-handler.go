b589853b1db0d7c9c88dfe22d2b9d837fb8bc027
kartik Verma <kartikx04@gmail.com>
2026-04-22 21:26:04 +0530

    added /ws route

--- internal/controllers/http-handler.go
+++ internal/controllers/http-handler.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/kartikx04/chat/internal/ws"
 	"github.com/kartikx04/chat/pkg"
 	"github.com/rs/cors"
 )
@@ -23,10 +24,12 @@ func StartHTTPServer() {
 		AllowCredentials: true,
 	})
 
-	handler := c.Handler(r)
-
 	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("ok")
+	})
+
+	r.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		ws.ServeWs(ws.HubInstance, w, r)
 	})
 
 	r.HandleFunc("/google-sso", GoogleSignOn)
@@ -38,5 +41,13 @@ func StartHTTPServer() {
 	r.HandleFunc("/verify-contact", verifyContactHandler)
 
 	log.Printf("Server running on :%s\n", pkg.LoadFile("SERVER_PORT"))
-	http.ListenAndServe(fmt.Sprintf(":%s", pkg.LoadFile("SERVER_PORT")), handler)
+	http.ListenAndServe(fmt.Sprintf(":%s", pkg.LoadFile("SERVER_PORT")),
+		http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			if req.URL.Path == "/ws" {
+				r.ServeHTTP(w, req) // 🚀 bypass CORS
+				return
+			}
+			c.Handler(r).ServeHTTP(w, req)
+		}),
+	)
 }
